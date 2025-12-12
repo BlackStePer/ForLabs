16a06ddc716d8b53a8e09ed51f5c21c28243a3e2
Adom1ne <adominetwel@gmail.com>
2025-12-13 00:23:35 +0700

    Add files via upload

---
+++ KuZIlya/PostOfficeVUZ/Postman.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostOfficeVUZ {
+    internal class Postman : Employee {
+        public Postman() => daysMonthYears = DaysMonthYears.Year;
+        public override string? WhatToDo() => "Разношу почту, мешайте";
+        /// <summary>
+        /// Метод, показывающий, сколько работает данный работник
+        /// </summary>
+        /// <returns>Кол-во лет</returns>
+        public override double WorkTime() => (DateTime.Now - DateOfEmplyment).TotalDays/365;
+        public override string ToString() => "поствщик";
+    }
+}
