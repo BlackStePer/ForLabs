286daa5edafd4238b254a5b6bb23178f8738cc97
Adom1ne <adominetwel@gmail.com>
2025-12-14 17:38:28 +0700

    Add files via upload

---
+++ KuZIlya/SmartphoneVuZ_Lab9/SmartphoneVuZ/FactoryClasses/GentleSmarphone.cs
@@ -0,0 +1,57 @@
+﻿using SmartphoneVuZ.Serialization;
+using System;
+using System.Numerics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SmartphoneVuZ.FactoryClasses {
+    /// <summary>
+    /// СуперСмартфон
+    /// </summary>
+    internal class GentleSmartphone
+    {
+        public static int currentID;
+        private static string path = "GentleSmartphoneID.json";
+        public static Serializer idSerializer = new Serializer(path);
+        public TactileSensor Sensor { get; private set; } = new TactileSensor();
+        public int SerialNumber { get; private set; }
+        [JsonConstructor]
+        private GentleSmartphone(int serialNumber, TactileSensor sensor)
+        {
+            SerialNumber = serialNumber;
+            Sensor = sensor;
+        }
+        static GentleSmartphone() // Получает данные о текущем currentID 
+        {
+            Deserializer deserializer = new Deserializer(path);
+            if (!deserializer.Deserialize(ref currentID))
+                currentID = 0;
+        }
+        public GentleSmartphone()
+        {
+            SerialNumber = currentID;
+            ++currentID;
+        }
+        // Тут я пытался заставить работать List<>.Contains(), как обычно, не получилось
+
+        //public override int GetHashCode() => SerialNumber;
+        //public bool Equals(GentleSmartphone smartphone)
+        //{
+        //    if (smartphone == null) return false;
+        //    return SerialNumber == smartphone.SerialNumber && Sensor.Sensitivity == smartphone.Sensor.Sensitivity;
+        //}
+        //public static bool operator== (GentleSmartphone smartphone1, GentleSmartphone smartphone2)
+        //{
+        //    if (ReferenceEquals(smartphone1, null) || ReferenceEquals(smartphone2, null))
+        //        return false;
+        //    return smartphone1.SerialNumber == smartphone2.SerialNumber && smartphone1.Sensor.Sensitivity == smartphone2.Sensor.Sensitivity;
+        //}
+        //public static bool operator!= (GentleSmartphone smartphone1, GentleSmartphone smartphone2)
+        //{
+        //    if (ReferenceEquals(smartphone1, null) || ReferenceEquals(smartphone2, null))
+        //        return false;
+        //    return smartphone1.SerialNumber != smartphone2.SerialNumber && smartphone1.Sensor.Sensitivity != smartphone2.Sensor.Sensitivity;
+        //}
+        public override string ToString() => $"Суперсмартфон{SerialNumber} с чувствительность {Sensor.Sensitivity}";
+    }
+}
---
+++ KuZIlya/SmartphoneVuZ_Lab9/SmartphoneVuZ/Observers/СompatibilityChecker.cs
@@ -0,0 +1,55 @@
+﻿using SmartphoneVuZ.FactoryClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartphoneVuZ.Observers {
+    /// <summary>
+    /// Подбирает клиенту телефон
+    /// </summary>
+    internal class СompatibilityChecker {
+        public Customer Customer { get; private set; }
+        public FactoryInformator Informator { get; private set; }
+        public List<GentleSmartphone> Smartphones { get; private set; }
+        public СompatibilityChecker(Customer customer, FactoryInformator informator, List<GentleSmartphone> smartphones)
+        {
+            Customer = customer;
+            Informator = informator;
+            Smartphones = smartphones;
+        }
+        /// <summary>
+        /// Проверяет, подходит ли телефон конкретному клиенту. Если находит подходящий телефон (или подходящий телефон + трансформатор), то продаёт его
+        /// </summary>
+        public void Check()
+        {
+            foreach (var smartphone in Smartphones) // Подбор без трансформатора
+                if (Customer.GentleRate / smartphone.Sensor.Sensitivity >= 1 && Customer.GentleRate / smartphone.Sensor.Sensitivity <= 1.5 ||
+                        smartphone.Sensor.Sensitivity / Customer.GentleRate >= 1 && smartphone.Sensor.Sensitivity / Customer.GentleRate <= 1.5)
+                {
+                    Customer.Smartphone = smartphone;
+                    Informator.RemoveObserver(Customer);
+                    Smartphones.Remove(smartphone);
+                    return;
+                }
+            foreach (var smartphone in Smartphones) // Подбор с трансформатором
+                if (Customer.GentleRate / smartphone.Sensor.Sensitivity > 1.5 && Customer.GentleRate / smartphone.Sensor.Sensitivity <= 2)
+                {
+                    Customer.Smartphone = smartphone;
+                    Customer.TransformModule = new Transformator(TransformatorType.Multiplier);
+                    Informator.RemoveObserver(Customer);
+                    Smartphones.Remove(smartphone);
+                    break;
+                }
+                else if (smartphone.Sensor.Sensitivity / Customer.GentleRate > 1.5 && smartphone.Sensor.Sensitivity / Customer.GentleRate <= 2)
+                {
+                    Customer.Smartphone = smartphone;
+                    Customer.TransformModule = new Transformator(TransformatorType.Devider);
+                    Informator.RemoveObserver(Customer);
+                    Smartphones.Remove(smartphone);
+                    break;
+                }
+        }
+    }
+}
