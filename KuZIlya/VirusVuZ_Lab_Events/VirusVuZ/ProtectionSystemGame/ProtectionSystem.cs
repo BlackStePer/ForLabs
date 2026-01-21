b8e5ed8b437932488e62f4baa8b9a504f6e8a3a3
Adom1ne <adominetwel@gmail.com>
2026-01-21 14:55:16 +0700

    Add files via upload

---
+++ KuZIlya/VirusVuZ_Lab_Events/VirusVuZ/ProtectionSystemGame/ProtectionSystem.cs
@@ -0,0 +1,46 @@
+﻿namespace VirusVuZ.ProtectionSystemGame
+{
+    /// <summary>
+    /// Класс, описывает системы безопасности
+    /// </summary>
+    internal abstract class ProtectionSystem
+    {
+        public string Title { get; set; }
+        public abstract List<ProtectionLayer> Layers { get; }
+        public DateTime Date { get; protected set; }
+        public abstract int ProtectionLayerNumber { get; }
+        public abstract int FalledProtectionLayerNumber { get; }
+        public ProtectionSystem(string title)
+        {
+            Title = title;
+            Date = DateTime.Now;
+        }
+        /// <summary>
+        /// Продержалась ли система дополнительный день
+        /// </summary>
+        public virtual void ProtectionCheck() => Date = Date.AddDays(1);
+        /// <summary>
+        /// При 0 хп слоя определяет, падёт он или нет
+        /// </summary>
+        /// <returns></returns>
+        public virtual bool GetAttack()
+        {
+            Random rand = new();
+            bool isBreached = rand.Next(100) < 50;
+            if (!isBreached)
+                ProtectionCheck();
+            return isBreached;
+        }
+        /// <summary>
+        /// При 0 хп слоя его падение определяется через value
+        /// </summary>
+        /// <param name="value">Падёт слой или нет</param>
+        /// <returns>Пал слой или нет</returns>
+        public bool GetAttack(bool value)
+        {
+            if (!value)
+                ProtectionCheck();
+            return value;
+        }
+    }
+}
